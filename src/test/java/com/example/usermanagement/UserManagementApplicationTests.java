7695f9d0af1addd6f0b03482d06e5c5fdf0737aa
Malek <bengharbiamohamedmalek@gmail.com>
2023-01-29 23:26:22 +0100

    pipeTest

--- src/test/java/com/example/usermanagement/UserManagementApplicationTests.java
+++ src/test/java/com/example/usermanagement/UserManagementApplicationTests.java
@@ -1,10 +1,28 @@
 package com.example.usermanagement;
 
+import com.example.usermanagement.model.User;
+import com.example.usermanagement.repository.UserRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
 class UserManagementApplicationTests {
+
+  @Autowired
+  UserRepository userRepository ;
+  @Test
+  public void addUserTest (){
+    User user = new User();
+    user.setAge(26);
+    user.setPrenom("test");
+    user.setNom("test");
+    user.setProfession("test");
+    userRepository.save(user);
+    assertNotNull(user.getUserId());
+  }
 
   @Test
   void contextLoads() {
--- src/test/java/com/example/usermanagement/model/UserTest.java
+++
@@ -1,7 +0,0 @@
-package com.example.usermanagement.model;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class UserTest {
-
-}
